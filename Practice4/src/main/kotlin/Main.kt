5382263fb923914558f3ad8dc77c93cd7d544251
GwynBlei <144176510+GwynBlei@users.noreply.github.com>
2023-09-20 11:40:33 +0300

    Update Main.kt

--- Practice4/src/main/kotlin/Main.kt
+++ Practice4/src/main/kotlin/Main.kt
@@ -28,3 +28,19 @@ fun calculateFullName(firstName: String,lastName: String) = firstName + lastName
 fun calculateFullName2(firstName: String,lastName: String):Pair<String,Int> {
     return Pair("$firstName $lastName", firstName.length + lastName.length)
 }
+//задание 5
+fun primeNumber (number: Int) : Boolean
+{
+    for (i in 2 .. (number/2))
+    {
+        if (number % i == 0)
+            return false
+    }
+    return true
+}
+//задание6
+fun fibonacci(n: Int): Int
+{
+    if (n == 1 || n == 2) return 1
+    else return fibonacci(n - 1) + fibonacci(n - 2)
+}
